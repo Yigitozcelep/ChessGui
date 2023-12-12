7aae3209ad3775d163954e88ec8b79649033f0c4
Yigit Ozcelep <yoroso85onurlu@outlook.com>
2023-12-12 21:07:10 +0300

    board can be visible

--- src/board.js
+++ src/board.js
@@ -1,10 +1,29 @@
 import { EngineController } from "./engine_controller.js";
+import { MenuButtonController, Colors } from "./main_menu.js";
 
-const invoke = window.__TAURI__.invoke
-const listen = window.__TAURI__.event.listen;
+const BOARD_CONTAINER = document.getElementById("board_container");
+const PIECES_DIV      = document.getElementById("pieces_container");
+const SQUARES_DIV     = document.getElementById("square_container");
 
-const BOARD = document.getElementById("board")
-const FILES = ["a","b","c","d","e","f","g","h"]
+const FILES           = ["a","b","c","d","e","f","g","h"]
+const BoardImg        = document.getElementById("board_img") ;
+
+const letterToPiece = {
+    "k": "black_king",
+    "q": "black_queen",
+    "n": "black_knight",
+    "p": "black_pawn",
+    "b": "black_bishop",
+    "r": "black_rook",
+    "K": "white_king",
+    "Q": "white_queen",
+    "N": "white_knight",
+    "P": "white_pawn",
+    "B": "white_bishop",
+    "R": "white_rook",
+}
+
+const isNumeric = (value) => /^-?\d+$/.test(value)
 
 
 class EnginePlayer {
@@ -23,8 +42,126 @@ class TimeController {
     }
 }
 
-class BoardController {
-    constructor() {
+class BoardConfigs {
+    constructor(isFlippedBoard=false, boardLeft=10, boardTop=9, boardSize=72) {
+        this.isFlippedBoard                  = isFlippedBoard;
+        this._boardLeft                      = boardLeft;
+        this._boardTop                       = boardTop;
+        this._boardSize                      = boardSize;
+        this._squareSize                     = this._boardSize  / 8;
+        this._pieceSize                      = this._squareSize * 0.75;
+        
+    }
 
+    getBoardLeft()                 { return this._boardLeft  + "vw";                 }
+    getBoardTop()                  { return this._boardTop   + "vw";                 }
+    getBoardSize()                 { return this._boardSize  + "vw";                 }
+    getSquareSize()                { return this._squareSize + "vw";                 }
+    getPieceSize()                 { return this._pieceSize  + "vw";                 }
+    getSquareDiv(squareName)       { return document.getElementById(squareName);     }
+    getSquareLeft(file)            { return (this._boardLeft + (this._squareSize * file))      + "vw";  }
+    getSquareTop(rank)             { return (this._boardTop + (this._squareSize * (7 - rank))) + "vw";  }
+    
+    initializeSquares() {
+        for (let fileIndex = 7; fileIndex >= 0; fileIndex--) {
+            for (let rankIndex = 7; rankIndex >= 0; rankIndex--) {
+                const file = FILES[fileIndex];
+                const square = document.createElement("div");
+                square.classList.add("square");
+                square.id = file + (rankIndex + 1);
+                square.style.height = square.style.width = this.getSquareSize();
+                square.style.left   = this.getSquareLeft(fileIndex);
+                square.style.top    = this.getSquareTop(rankIndex);
+                SQUARES_DIV.appendChild(square);
+            }
+        }
+    }
+
+    initialize() {
+        BOARD_CONTAINER.style.visibility = "visible";
+        BoardImg.style.visibility = "visible";
+        BoardImg.style.width  = this.getBoardSize();
+        BoardImg.style.height = this.getBoardSize();
+        BoardImg.style.left   = this.getBoardLeft();
+        BoardImg.style.top    = this.getBoardTop();
+        this.initializeSquares();
     }
 }
+
+class GameState {
+    constructor() {
+        this._fen  = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        this._turn = 0;
+        this._moveCount = 0;
+    }
+    setFen(fen)       { this._fen  = fen;                                }
+    setTurn(turn)     { this._turn = turn;                              }
+    getFen()          { return this._fen;                               }
+    getTurnIndex()    { return this._turn;                              }
+    getPiecesLayout() { return this.getFen().split(" ")[0]              }
+    getColor()        { return this._turn ? Colors.black : Colors.white }
+}
+
+const boardEvents = {
+    clickPieceObservers  : [],
+    movedPlayedObservers : [],
+    gameFnishedObservers : [],
+    firstMoveObservers   : [],
+    undoObservers        : [],
+    redoObservers        : [],
+}
+
+const boardStates = {
+    configs         : new BoardConfigs(),
+    gameState       : new GameState(),
+    boardStrategy   : 2,
+
+    createItem(square, piece, moves, pieceColor) {
+        const rank = Math.floor(square / 8);
+        const file = square % 8;
+        const fileName = "./svgs/" + piece + ".svg"
+        
+        let img = document.createElement("img");
+        img.src = fileName;
+        img.classList.add("piece");
+        img.id = (FILES[file] + (rank + 1));
+      
+        
+        // if (isGrabbable(pieceColor)) img.classList.add("grabbable")
+        
+        img.pieceName     = piece;
+        img.currentSquare = square;
+        img.currentLeft   = img.style.left;
+        img.currentTop    = img.style.top;
+        img.currentMoves  = moves
+        
+        PIECES_DIV.appendChild(img);
+    },
+
+    createPieces() {
+        let square = 56;
+        const piecesLayout = this.gameState.getPiecesLayout();
+        for (let i = 0; i < piecesLayout.length; i++) {
+          if (isNumeric(piecesLayout[i])) { square += parseInt(piecesLayout[i]); }
+          else if (piecesLayout[i] == "/") square += -16;
+          else {
+            this.createItem(square, letterToPiece[piecesLayout[i]], currentMoves);
+            square += 1;
+          }
+       }
+    },
+    
+    initialize() {
+        MenuButtonController.addObserver(this);
+        MenuButtonController.makeVisible();
+        this.configs.initialize();
+        this.createPieces();
+    },
+
+    notify() {
+        BoardImg.style.visibility = "hidden";
+    }
+}
+
+
+export { boardStates, BoardConfigs, GameState }
--- src/main_menu.js
+++ src/main_menu.js
@@ -1,14 +1,5 @@
 import { EngineController, searchData } from "./engine_controller.js"
-
-const save =  window.__TAURI__.dialog.save;
-
-
-async function Something() {
-    const filePath = await dialog.open({ multiple: false, directory: false });
-    if (filePath) {
-        console.log(`Selected file: ${filePath}`);
-    } 
-}
+import { boardStates } from "./board.js";
 
 const SetBlackPlayerButton     = document.getElementById("set_black_player_button");
 const setWhitePlayerButton     = document.getElementById("set_white_player_button");
@@ -20,10 +11,20 @@ const BtimeInput               = document.getElementById("black_time_entry");
 const MainEngineDiv            = document.getElementById("main_engine_div");
 const SaveEngineDiv            = document.getElementById("save_engine");
 const DeleteEngineDiv          = document.getElementById("delete_engine_container");
+const PlayerVsPlayerButton     = document.getElementById("PlayerVsPlayer");
+const PlayerVsEngineButton     = document.getElementById("PlayerVsEngine");
+const EngineVsEngineButton     = document.getElementById("EngineVsEngine");
+const TestEngineButton         = document.getElementById("TestEngine");
+const MenuContainer            = document.getElementById("menu_container");
+const MenuButton               = document.getElementById("menu_button");
 
 SaveEngineDiv.onclick        = () => EngineController.saveEngine();
 setWhitePlayerButton.onclick = () => setColorOption("white")
 SetBlackPlayerButton.onclick = () => setColorOption("black")
+PlayerVsEngineButton.onclick = () => {
+    boardStates.initialize()
+    MenuContainer.style.visibility = "hidden";
+};
 
 const PushEnginesNamesToDiv = (div) => {
     div.innerHTML = "";
@@ -46,58 +47,24 @@ const PushEnginesNamesToDiv = (div) => {
 document.getElementById("main_engine_top_name").onclick = () => PushEnginesNamesToDiv(MainEngineDiv)
 DeleteEngineDiv.onclick = () => EngineController.deleteEngine() 
 
-const MainMenu = {
+const MenuButtonController = {
     observers: [],
-    menuDiv: document.getElementById("go_back_main_menu"),
     getClickedEvent()        {   return "main_menu_button_clicked"                            },
     addObserver(observer)    {  this.observers.push(observer)                                 },
     removeObserver(observer) {  this.observers = this.observers.filter(el => el != observer)  },
     clickMainMenu() {
         for (let observer of this.observers) observer.notify(this.getClickedEvent());
-        this.menuDiv.style.visibility = "hidden";
+        MenuContainer.style.visibility = "visible";
+        MenuButton.style.visibility = "hidden";
     },
-    makeVisible() { this.menuDiv.style.visibility = "visible"; }
+    makeVisible() { MenuButton.style.visibility = "visible" }
 }
 
-MainMenu.menuDiv.onclick = () => { MainMenu.clickMainMenu() }
+MenuButton.onclick = () => { MenuButtonController.clickMainMenu() }
 
 const Colors = {
     white: "white",
     black: "black",
-}
-
-class EnginePlayer {
-    constructor(id) { this.id = id}
-    makeMove() {}
-}
-
-class HumanPlayer {
-    constructor() {}
-    makeMove() {}
-}
-
-class BoardBuilder {
-    constructor() {
-        this.searchData = new searchData().setWtime(getWtime()).setBtime(getBtime()).setWinc(getWinc()).setBinc(getBinc());
-        this.players                        = [new HumanPlayer(), new HumanPlayer()];
-        this.isFlippedBoard                 = false;
-        this.pieceWidth                     = 6;
-        this.pieceHeight                    = 6;
-        this.boardLeft                      = 10;
-        this.boardTop                       = 9;
-        this.squareWidth                    = 9;
-        this.squareHeight                   = 9;
-        this.moveAffectSpeed                = 250;
-        this.updateTimeInterval             = 200;
-        this.opacityOfNoneTurnPlayerTimeDiv = "0.7";
-    }
-
-    addEnginePlayer(player, index) {  this.players[index] = player; return this;  }
-    setPieceWidth(width)           {  this.pieceWidth     = width;  return this;  }
-    setPieceHeight(height)         {  this.pieceHeight    = height; return this;  }
-    setBoardLeft(left)             {  this.boardLeft      = left;   return this;  }
-    setBoardTop(top)               {  this.boardTop       = top;    return this;  }
-    build() {}
 }
 
 const setColorOption = (color) => {
@@ -114,4 +81,4 @@ const getBinc   = () => BincInput.value.trim()
 const getWtime  = () => WtimeInput.value.trim();
 const getBtime  = () => BtimeInput.value.trim();
 
-export {MainMenu}
+export { MenuButtonController, Colors }
